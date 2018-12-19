8b698a2f2c8588111749eb33a039b666d2bb40ae
MegaShow <megaxiu@outlook.com>
2018-12-19 20:45:42 +0800

    Recode alpha-beta pruning

--- chess/chess.go
+++ chess/chess.go
@@ -1,24 +1,100 @@
 package chess
 
 import (
+	"strings"
 	"unicode"
 )
 
 // Chess 棋盘信息
 type Chess struct {
-	Data [10][9]byte
-	Next byte
+	Board map[int]Piece
+	Next  byte
+	Size  int
+}
+
+type Piece struct {
+	Type        int
+	Index       int // Index = NumberIndex * 9 + AlphaIndex
+	AlphaIndex  int
+	NumberIndex int
+}
+
+func NewChess(fen string, steps []string) *Chess {
+	strArgs := strings.Split(fen, " ")
+	lines := strings.Split(strArgs[0], "/")
+	chess := Chess{
+		Board: make(map[int]Piece),
+		Next:  byte(strArgs[1][0]),
+		Size:  0,
+	}
+	for i := 0; i < len(lines); i++ {
+		var index int
+		for _, v := range lines[len(lines)-1-i] {
+			if v >= '1' && v <= '9' {
+				index += int(v - '0')
+			} else {
+				chess.Board[i*9+index] = Piece{
+					Type:        int(v),
+					Index:       i*9 + index,
+					AlphaIndex:  index,
+					NumberIndex: i,
+				}
+				index++
+			}
+		}
+	}
+	for _, step := range steps {
+		chess.Move([4]int{int(step[1] - '0'), int(step[0] - 'a'), int(step[3] - '0'), int(step[2] - 'a')})
+	}
+	return &chess
+}
+
+func (c *Chess) GetPiecesSize() int {
+	return len(c.Board)
+}
+
+func (c *Chess) IsWin(player byte) bool {
+	return false
+}
+
+func (c *Chess) Evaluate(steps [][4]int) int {
+	var redValue, blackValue int
+	var redBasicValue, blackBasicValue int
+	var redPositionValue, blackPositionValue int
+	for _, p := range c.Board {
+		positions, ok := PositionValueTable[byte(unicode.ToLower(rune(p.Type)))]
+		if p.Type >= 'a' && p.Type <= 'z' {
+			blackBasicValue += BasicValueTable[p.Type]
+			if ok {
+				blackPositionValue += positions[p.NumberIndex][p.AlphaIndex]
+			}
+		} else {
+			redBasicValue += BasicValueTable[p.Type]
+			if ok {
+				redPositionValue += positions[9-p.NumberIndex][p.AlphaIndex]
+			}
+		}
+	}
+	redValue = redBasicValue + redPositionValue*8
+	blackValue = blackBasicValue + blackPositionValue*8
+	// fmt.Println(redValue, blackValue, redBasicValue, redPositionValue, blackBasicValue, blackPositionValue)
+	if c.Next == 'b' {
+		return blackValue - redValue
+	} else {
+		return redValue - blackValue
+	}
 }
 
 // 子力价值
-var BasicValueTable = map[byte]int{
+var BasicValueTable = map[int]int{
 	// 小写 - 黑, 大写 - 红
 	// k - 将, a - 士, b - 象, n - 马, r - 车, c - 炮, p - 兵
-	'k': 10000, 'a': 110, 'b': 110, 'n': 300, 'r': 600, 'c': 300, 'p': 70,
+	'k': 100000, 'a': 110, 'b': 110, 'n': 300, 'r': 600, 'c': 300, 'p': 70,
+	'K': 100000, 'A': 110, 'B': 110, 'N': 300, 'R': 600, 'C': 300, 'P': 70,
 }
 
 // 机动性价值
-var MobilityValueTable = map[byte]int{
+var MobilityValueTable = map[int]int{
 	'k': 0, 'a': 1, 'b': 1, 'n': 13, 'r': 7, 'c': 7, 'p': 15,
 	'K': 0, 'A': 1, 'B': 1, 'N': 13, 'R': 7, 'C': 7, 'P': 15,
 }
@@ -73,393 +149,4 @@ var PositionValueTable = map[byte][10][9]int{
 		{0, 2, 4, 6, 6, 6, 4, 2, 0},
 		{0, 0, 2, 6, 6, 6, 2, 0, 0},
 	},
-}
-
-func evaluate(chess *Chess) int {
-	var redValue, blackValue int
-	var redBasicValue, blackBasicValue int
-	var redPositionValue, blackPositionValue int
-	for n := 0; n < len(chess.Data); n++ {
-		for a := 0; a < len(chess.Data[n]); a++ {
-			positions := PositionValueTable[byte(unicode.ToLower(rune(chess.Data[n][a])))]
-			if chess.Next == 'b' {
-				blackBasicValue += BasicValueTable[chess.Data[n][a]]
-				blackPositionValue += positions[n][a]
-			} else {
-				redBasicValue += BasicValueTable[byte(unicode.ToLower(rune(chess.Data[n][a])))]
-				redPositionValue += positions[9-n][a]
-			}
-		}
-	}
-	redValue = redBasicValue + redPositionValue
-	blackValue = blackBasicValue + blackBasicValue
-	if chess.Next == 'b' {
-		return blackValue - redValue
-	} else {
-		return redValue - blackValue
-	}
-}
-
-func checkStep(chess *Chess, steps *[][4]int, oldA, oldN, newA, newN int) {
-	if newN < len(chess.Data) && newA < len(chess.Data[newN]) &&
-		(chess.Data[newN][newA] == 0 || (chess.Next == 'b' && unicode.IsUpper(rune(chess.Data[newN][newA]))) ||
-			(chess.Next != 'b' && unicode.IsLower(rune(chess.Data[newN][newA])))) {
-		*steps = append(*steps, [4]int{oldA, oldN, newA, newN})
-	}
-}
-
-func createNextStep(chess *Chess) (steps [][4]int) {
-	if chess.Next == 'b' {
-		for n := 0; n < len(chess.Data); n++ {
-			for a := 0; a < len(chess.Data[n]); a++ {
-				if chess.Data[n][a] == 'k' { // 将
-					if n >= 8 {
-						checkStep(chess, &steps, a, n, a, n-1)
-					}
-					if n <= 8 {
-						checkStep(chess, &steps, a, n, a, n+1)
-					}
-					if a >= 'e'-'a' {
-						checkStep(chess, &steps, a, n, a-1, n)
-					}
-					if a <= 'e'-'a' {
-						checkStep(chess, &steps, a, n, a+1, n)
-					}
-				} else if chess.Data[n][a] == 'a' { // 士
-					if n == 8 {
-						checkStep(chess, &steps, a, n, 'd'-'a', 7)
-						checkStep(chess, &steps, a, n, 'd'-'a', 9)
-						checkStep(chess, &steps, a, n, 'f'-'a', 7)
-						checkStep(chess, &steps, a, n, 'f'-'a', 9)
-					} else {
-						checkStep(chess, &steps, a, n, 'e'-'a', 8)
-					}
-				} else if chess.Data[n][a] == 'b' { // 象
-					if a == 'a'-'a' {
-						checkStep(chess, &steps, a, n, 'c'-'a', 5)
-						checkStep(chess, &steps, a, n, 'c'-'a', 9)
-					} else if a == 'c'-'a' {
-						checkStep(chess, &steps, a, n, 'a'-'a', 7)
-						checkStep(chess, &steps, a, n, 'e'-'a', 7)
-					} else if a == 'e'-'a' {
-						checkStep(chess, &steps, a, n, 'c'-'a', 5)
-						checkStep(chess, &steps, a, n, 'c'-'a', 9)
-						checkStep(chess, &steps, a, n, 'g'-'a', 5)
-						checkStep(chess, &steps, a, n, 'g'-'a', 9)
-					} else if a == 'g'-'a' {
-						checkStep(chess, &steps, a, n, 'e'-'a', 7)
-						checkStep(chess, &steps, a, n, 'i'-'a', 7)
-					} else if a == 'i'-'a' {
-						checkStep(chess, &steps, a, n, 'g'-'a', 5)
-						checkStep(chess, &steps, a, n, 'g'-'a', 9)
-					}
-				} else if chess.Data[n][a] == 'n' {
-					if n+2 < len(chess.Data) && a+1 < len(chess.Data[n]) && chess.Data[n+1][a] == 0 {
-						checkStep(chess, &steps, a, n, a+1, n+2)
-					}
-					if n+1 < len(chess.Data) && a+2 < len(chess.Data[n]) && chess.Data[n][a+1] == 0 {
-						checkStep(chess, &steps, a, n, a+2, n+1)
-					}
-					if n-1 >= 0 && a+2 < len(chess.Data[n]) && chess.Data[n][a+1] == 0 {
-						checkStep(chess, &steps, a, n, a+2, n-1)
-					}
-					if n-2 >= 0 && a+1 < len(chess.Data[n]) && chess.Data[n-1][a] == 0 {
-						checkStep(chess, &steps, a, n, a+1, n-2)
-					}
-					if n-2 >= 0 && a-1 >= 0 && chess.Data[n-1][a] == 0 {
-						checkStep(chess, &steps, a, n, a-1, n-2)
-					}
-					if n-1 >= 0 && a-2 >= 0 && chess.Data[n][a-1] == 0 {
-						checkStep(chess, &steps, a, n, a-2, n-1)
-					}
-					if n+1 < len(chess.Data) && a-2 >= 0 && chess.Data[n][a-1] == 0 {
-						checkStep(chess, &steps, a, n, a+2, n-1)
-					}
-					if n+2 < len(chess.Data) && a-1 >= 0 && chess.Data[n+1][a] == 0 {
-						checkStep(chess, &steps, a, n, a-1, n+2)
-					}
-				} else if chess.Data[n][a] == 'r' { // 车
-					for i := 1; n+i < len(chess.Data); i++ {
-						checkStep(chess, &steps, a, n, a, n+i)
-						if chess.Data[n+i][a] != 0 {
-							break
-						}
-					}
-					for i := 1; n-i >= 0; i++ {
-						checkStep(chess, &steps, a, n, a, n-i)
-						if chess.Data[n-i][a] != 0 {
-							break
-						}
-					}
-					for i := 1; a+i < len(chess.Data[n]); i++ {
-						checkStep(chess, &steps, a, n, a+i, n)
-						if chess.Data[n][a+i] != 0 {
-							break
-						}
-					}
-					for i := 1; a-i >= 0; i++ {
-						checkStep(chess, &steps, a, n, a-i, n)
-						if chess.Data[n][a-i] != 0 {
-							break
-						}
-					}
-				} else if chess.Data[n][a] == 'c' { // 炮
-					var i int
-					for i = 1; n+i < len(chess.Data) && chess.Data[n+i][a] == 0; i++ {
-						checkStep(chess, &steps, a, n, a, n+i)
-					}
-					for i++; n+i < len(chess.Data); i++ {
-						if chess.Data[n+i][a] != 0 {
-							checkStep(chess, &steps, a, n, a, n+i)
-							break
-						}
-					}
-					for i = 1; n-i >= 0 && chess.Data[n-i][a] == 0; i++ {
-						checkStep(chess, &steps, a, n, a, n-i)
-					}
-					for i++; n-i >= 0; i++ {
-						if chess.Data[n-i][a] != 0 {
-							checkStep(chess, &steps, a, n, a, n-i)
-							break
-						}
-					}
-					for i = 1; a+i < len(chess.Data[n]) && chess.Data[n][a+i] == 0; i++ {
-						checkStep(chess, &steps, a, n, a+i, n)
-					}
-					for i++; a+i < len(chess.Data[n]); i++ {
-						if chess.Data[n][a+i] != 0 {
-							checkStep(chess, &steps, a, n, a+i, n)
-							break
-						}
-					}
-					for i = 1; a-i >= 0 && chess.Data[n][a-i] == 0; i++ {
-						checkStep(chess, &steps, a, n, a-i, n)
-					}
-					for i++; a-i >= 0; i++ {
-						if chess.Data[n][a-i] != 0 {
-							checkStep(chess, &steps, a, n, a-i, n)
-							break
-						}
-					}
-				} else if chess.Data[n][a] == 'p' { // 兵
-					if n == 6 || n == 5 {
-						checkStep(chess, &steps, a, n, a, n-1)
-					} else {
-						checkStep(chess, &steps, a, n, a, n-1)
-						checkStep(chess, &steps, a, n, a-1, n)
-						checkStep(chess, &steps, a, n, a+1, n)
-					}
-				}
-			}
-		}
-	} else {
-		for n := 0; n < len(chess.Data); n++ {
-			for a := 0; a < len(chess.Data[n]); a++ {
-				if chess.Data[n][a] == 'K' { // 帅
-					if n >= 1 {
-						checkStep(chess, &steps, a, n, a, n-1)
-					}
-					if n <= 1 {
-						checkStep(chess, &steps, a, n, a, n+1)
-					}
-					if a >= 'e'-'a' {
-						checkStep(chess, &steps, a, n, a-1, n)
-					}
-					if a <= 'e'-'a' {
-						checkStep(chess, &steps, a, n, a+1, n)
-					}
-				} else if chess.Data[n][a] == 'A' { // 仕
-					if n == 1 {
-						checkStep(chess, &steps, a, n, 'd'-'a', 0)
-						checkStep(chess, &steps, a, n, 'd'-'a', 2)
-						checkStep(chess, &steps, a, n, 'f'-'a', 0)
-						checkStep(chess, &steps, a, n, 'f'-'a', 2)
-					} else {
-						checkStep(chess, &steps, a, n, 'e'-'a', 1)
-					}
-				} else if chess.Data[n][a] == 'B' { // 相
-					if a == 'a'-'a' {
-						checkStep(chess, &steps, a, n, 'c'-'a', 0)
-						checkStep(chess, &steps, a, n, 'c'-'a', 4)
-					} else if a == 'c'-'a' {
-						checkStep(chess, &steps, a, n, 'a'-'a', 2)
-						checkStep(chess, &steps, a, n, 'e'-'a', 2)
-					} else if a == 'e'-'a' {
-						checkStep(chess, &steps, a, n, 'c'-'a', 0)
-						checkStep(chess, &steps, a, n, 'c'-'a', 4)
-						checkStep(chess, &steps, a, n, 'g'-'a', 0)
-						checkStep(chess, &steps, a, n, 'g'-'a', 4)
-					} else if a == 'g'-'a' {
-						checkStep(chess, &steps, a, n, 'e'-'a', 2)
-						checkStep(chess, &steps, a, n, 'i'-'a', 2)
-					} else if a == 'i'-'a' {
-						checkStep(chess, &steps, a, n, 'g'-'a', 0)
-						checkStep(chess, &steps, a, n, 'g'-'a', 4)
-					}
-				} else if chess.Data[n][a] == 'N' {
-					if n+2 < len(chess.Data) && a+1 < len(chess.Data[n]) && chess.Data[n+1][a] == 0 {
-						checkStep(chess, &steps, a, n, a+1, n+2)
-					}
-					if n+1 < len(chess.Data) && a+2 < len(chess.Data[n]) && chess.Data[n][a+1] == 0 {
-						checkStep(chess, &steps, a, n, a+2, n+1)
-					}
-					if n-1 >= 0 && a+2 < len(chess.Data[n]) && chess.Data[n][a+1] == 0 {
-						checkStep(chess, &steps, a, n, a+2, n-1)
-					}
-					if n-2 >= 0 && a+1 < len(chess.Data[n]) && chess.Data[n-1][a] == 0 {
-						checkStep(chess, &steps, a, n, a+1, n-2)
-					}
-					if n-2 >= 0 && a-1 >= 0 && chess.Data[n-1][a] == 0 {
-						checkStep(chess, &steps, a, n, a-1, n-2)
-					}
-					if n-1 >= 0 && a-2 >= 0 && chess.Data[n][a-1] == 0 {
-						checkStep(chess, &steps, a, n, a-2, n-1)
-					}
-					if n+1 < len(chess.Data) && a-2 >= 0 && chess.Data[n][a-1] == 0 {
-						checkStep(chess, &steps, a, n, a-2, n+1)
-					}
-					if n+2 < len(chess.Data) && a-1 >= 0 && chess.Data[n+1][a] == 0 {
-						checkStep(chess, &steps, a, n, a-1, n+2)
-					}
-				} else if chess.Data[n][a] == 'R' { // 车
-					for i := 1; n+i < len(chess.Data); i++ {
-						checkStep(chess, &steps, a, n, a, n+i)
-						if chess.Data[n+i][a] != 0 {
-							break
-						}
-					}
-					for i := 1; n-i >= 0; i++ {
-						checkStep(chess, &steps, a, n, a, n-i)
-						if chess.Data[n-i][a] != 0 {
-							break
-						}
-					}
-					for i := 1; a+i < len(chess.Data[n]); i++ {
-						checkStep(chess, &steps, a, n, a+i, n)
-						if chess.Data[n][a+i] != 0 {
-							break
-						}
-					}
-					for i := 1; a-i >= 0; i++ {
-						checkStep(chess, &steps, a, n, a-i, n)
-						if chess.Data[n][a-i] != 0 {
-							break
-						}
-					}
-				} else if chess.Data[n][a] == 'C' { // 炮
-					var i int
-					for i = 1; n+i < len(chess.Data) && chess.Data[n+i][a] == 0; i++ {
-						checkStep(chess, &steps, a, n, a, n+i)
-					}
-					for i++; n+i < len(chess.Data); i++ {
-						if chess.Data[n+i][a] != 0 {
-							checkStep(chess, &steps, a, n, a, n+i)
-							break
-						}
-					}
-					for i = 1; n-i >= 0 && chess.Data[n-i][a] == 0; i++ {
-						checkStep(chess, &steps, a, n, a, n-i)
-					}
-					for i++; n-i >= 0; i++ {
-						if chess.Data[n-i][a] != 0 {
-							checkStep(chess, &steps, a, n, a, n-i)
-							break
-						}
-					}
-					for i = 1; a+i < len(chess.Data[n]) && chess.Data[n][a+i] == 0; i++ {
-						checkStep(chess, &steps, a, n, a+i, n)
-					}
-					for i++; a+i < len(chess.Data[n]); i++ {
-						if chess.Data[n][a+i] != 0 {
-							checkStep(chess, &steps, a, n, a+i, n)
-							break
-						}
-					}
-					for i = 1; a-i >= 0 && chess.Data[n][a-i] == 0; i++ {
-						checkStep(chess, &steps, a, n, a-i, n)
-					}
-					for i++; a-i >= 0; i++ {
-						if chess.Data[n][a-i] != 0 {
-							checkStep(chess, &steps, a, n, a-i, n)
-							break
-						}
-					}
-				} else if chess.Data[n][a] == 'P' { // 兵
-					if n == 3 || n == 4 {
-						checkStep(chess, &steps, a, n, a, n+1)
-					} else {
-						if n+1 < len(chess.Data) {
-							checkStep(chess, &steps, a, n, a, n+1)
-						}
-						if a-1 >= 0 {
-							checkStep(chess, &steps, a, n, a-1, n)
-						}
-						if a+1 < len(chess.Data[n]) {
-							checkStep(chess, &steps, a, n, a+1, n)
-						}
-					}
-				}
-			}
-		}
-	}
-	return
-}
-
-// AlphaBetaPruning alpha-beta剪枝
-func AlphaBetaPruning(chess *Chess, depth, alpha, beta int) (int, [4]int) {
-	if depth == 0 {
-		return evaluate(chess), [4]int{}
-	}
-	steps := createNextStep(chess)
-	// fmt.Println(chess.Next, depth, alpha, beta)
-	var old byte
-	var s [4]int
-	for _, step := range steps {
-		chess.Data[step[1]][step[0]], chess.Data[step[3]][step[2]], old = 0, chess.Data[step[1]][step[0]], chess.Data[step[3]][step[2]]
-		chess.Next = 'w'
-		v, _ := AlphaBetaPruning(chess, depth-1, -beta, -alpha)
-		v = -v
-		chess.Next = 'b'
-		chess.Data[step[1]][step[0]], chess.Data[step[3]][step[2]] = chess.Data[step[3]][step[2]], old
-		if v >= beta {
-			return beta, s
-		}
-		if v > alpha {
-			alpha = v
-			s = step
-		}
-	}
-	return alpha, s
-	//if chess.Next == 'b' { // 极大值层
-	//	for _, step := range steps {
-	//		chess.Data[step[1]][step[0]], chess.Data[step[3]][step[2]], old = 0, chess.Data[step[1]][step[0]], chess.Data[step[3]][step[2]]
-	//		chess.Next = 'w'
-	//		v, _ := AlphaBetaPruning(chess, depth-1, alpha, beta)
-	//		chess.Next = 'b'
-	//		chess.Data[step[1]][step[0]], chess.Data[step[3]][step[2]] = chess.Data[step[3]][step[2]], old
-	//		if v >= alpha {
-	//			alpha = v
-	//			s = step
-	//		}
-	//		// beta剪枝
-	//		if alpha >= beta {
-	//			break
-	//		}
-	//	}
-	//	return alpha, s
-	//} else { // 极小值层
-	//	for _, step := range steps {
-	//		chess.Data[step[1]][step[0]], chess.Data[step[3]][step[2]], old = 0, chess.Data[step[1]][step[0]], chess.Data[step[3]][step[2]]
-	//		chess.Next = 'b'
-	//		v, _ := AlphaBetaPruning(chess, depth-1, alpha, beta)
-	//		chess.Next = 'w'
-	//		chess.Data[step[1]][step[0]], chess.Data[step[3]][step[2]] = chess.Data[step[3]][step[2]], old
-	//		beta = int(math.Min(float64(v), float64(beta)))
-	//		// alpha剪枝
-	//		if alpha >= beta {
-	//			break
-	//		}
-	//	}
-	//	return beta, s
-	//}
 }
---
+++ chess/move.go
@@ -0,0 +1,203 @@
+package chess
+
+import "unicode"
+
+func (c *Chess) Move(step [4]int) (state [2]Piece) {
+	oldPiece := c.Board[step[0]*9+step[1]]
+	newPiece, ok := c.Board[step[2]*9+step[3]]
+	if !ok {
+		newPiece.Index = step[2]*9 + step[3]
+		newPiece.NumberIndex = step[2]
+		newPiece.AlphaIndex = step[3]
+	}
+	state[0], state[1] = oldPiece, newPiece
+	delete(c.Board, oldPiece.Index)
+	newPiece.Type = oldPiece.Type
+	c.Board[newPiece.Index] = newPiece
+	if c.Next == 'b' {
+		c.Next = 'w'
+	} else {
+		c.Next = 'b'
+	}
+	return
+}
+
+func (c *Chess) UnMove(state [2]Piece) {
+	c.Board[state[0].Index] = state[0]
+	if state[1].Type == 0 {
+		delete(c.Board, state[1].Index)
+	} else {
+		c.Board[state[1].Index] = state[1]
+	}
+	if c.Next == 'b' {
+		c.Next = 'w'
+	} else {
+		c.Next = 'b'
+	}
+}
+
+var KMoveRules = [4][2]int{{-1, 0}, {0, -1}, {0, 1}, {1, 0}}
+var AMoveRules = [4][2]int{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}}
+var BMoveRules = [4][2]int{{-2, -2}, {-2, 2}, {2, -2}, {2, 2}}
+var NMoveRules = [8][2]int{{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}}
+var PMoveRules = [3][2]int{{0, -1}, {0, 1}, {1, 0}}
+
+func checkStepAndSet(chess *Chess, steps *[][4]int, p *Piece, newN, newA int) {
+	newP, ok := chess.Board[newN*9+newA]
+	if !ok || (chess.Next == 'b' && unicode.IsUpper(rune(newP.Type))) || (chess.Next != 'b' && unicode.IsLower(rune(newP.Type))) {
+		*steps = append(*steps, [4]int{p.NumberIndex, p.AlphaIndex, newN, newA})
+	}
+}
+
+func (c *Chess) GetNextSteps() (steps [][4]int) {
+	for _, p := range c.Board {
+		if (c.Next == 'b' && unicode.IsUpper(rune(p.Type))) || (c.Next != 'b' && unicode.IsLower(rune(p.Type))) {
+			continue
+		}
+		switch unicode.ToLower(rune(p.Type)) {
+		case 'k': // 将、帅
+			for _, r := range KMoveRules {
+				if ((p.NumberIndex+r[0] >= 0 && p.NumberIndex+r[0] <= 2) || (p.NumberIndex+r[0] >= 7 && p.NumberIndex+r[0] <= 9)) &&
+					(p.AlphaIndex+r[1] >= 'd'-'a' && p.AlphaIndex+r[1] <= 'f'-'a') {
+					checkStepAndSet(c, &steps, &p, p.NumberIndex+r[0], p.AlphaIndex+r[1])
+				}
+			}
+		case 'a': // 士、仕
+			for _, r := range AMoveRules {
+				if ((p.NumberIndex+r[0] >= 0 && p.NumberIndex+r[0] <= 2) || (p.NumberIndex+r[0] >= 7 && p.NumberIndex+r[0] <= 9)) &&
+					(p.AlphaIndex+r[1] >= 'd'-'a' && p.AlphaIndex+r[1] <= 'f'-'a') {
+					checkStepAndSet(c, &steps, &p, p.NumberIndex+r[0], p.AlphaIndex+r[1])
+				}
+			}
+		case 'b': // 象、相
+			for _, r := range BMoveRules {
+				if c.Next == 'b' {
+					if p.NumberIndex+r[0]/2 >= 5 && p.NumberIndex+r[0]/2 <= 9 && p.AlphaIndex+r[1]/2 >= 'a'-'a' && p.AlphaIndex+r[1]/2 <= 'i'-'a' {
+						if _, ok := c.Board[(p.NumberIndex+r[0]/2)*9+p.AlphaIndex+r[1]/2]; !ok &&
+							p.NumberIndex+r[0] >= 5 && p.NumberIndex+r[0] <= 9 && p.AlphaIndex+r[1] >= 'a'-'a' && p.AlphaIndex+r[1] <= 'i'-'a' {
+							checkStepAndSet(c, &steps, &p, p.NumberIndex+r[0], p.AlphaIndex+r[1])
+						}
+					}
+				} else {
+					if p.NumberIndex+r[0]/2 >= 0 && p.NumberIndex+r[0]/2 <= 4 && p.AlphaIndex+r[1]/2 >= 'a'-'a' && p.AlphaIndex+r[1]/2 <= 'i'-'a' {
+						if _, ok := c.Board[(p.NumberIndex+r[0]/2)*9+p.AlphaIndex+r[1]/2]; !ok &&
+							p.NumberIndex+r[0] >= 0 && p.NumberIndex+r[0] <= 4 && p.AlphaIndex+r[1] >= 'a'-'a' && p.AlphaIndex+r[1] <= 'i'-'a' {
+							checkStepAndSet(c, &steps, &p, p.NumberIndex+r[0], p.AlphaIndex+r[1])
+						}
+					}
+				}
+			}
+		case 'n': // 马
+			for _, r := range NMoveRules {
+				if p.NumberIndex+r[0]/2 >= 0 && p.NumberIndex+r[0]/2 <= 9 && p.AlphaIndex+r[1]/2 >= 'a'-'a' && p.AlphaIndex+r[1]/2 <= 'i'-'a' {
+					if _, ok := c.Board[(p.NumberIndex+r[0]/2)*9+p.AlphaIndex+r[1]/2]; !ok &&
+						p.NumberIndex+r[0] >= 0 && p.NumberIndex+r[0] <= 9 && p.AlphaIndex+r[1] >= 'a'-'a' && p.AlphaIndex+r[1] <= 'i'-'a' {
+						checkStepAndSet(c, &steps, &p, p.NumberIndex+r[0], p.AlphaIndex+r[1])
+					}
+				}
+			}
+		case 'r': // 车
+			for i := 1; p.NumberIndex+i <= 9; i++ {
+				checkStepAndSet(c, &steps, &p, p.NumberIndex+i, p.AlphaIndex)
+				if _, ok := c.Board[(p.NumberIndex+i)*9+p.AlphaIndex]; ok {
+					break
+				}
+			}
+			for i := -1; p.NumberIndex+i >= 0; i-- {
+				checkStepAndSet(c, &steps, &p, p.NumberIndex+i, p.AlphaIndex)
+				if _, ok := c.Board[(p.NumberIndex+i)*9+p.AlphaIndex]; ok {
+					break
+				}
+			}
+			for i := 1; p.AlphaIndex+i <= 'i'-'a'; i++ {
+				checkStepAndSet(c, &steps, &p, p.NumberIndex, p.AlphaIndex+i)
+				if _, ok := c.Board[p.NumberIndex*9+p.AlphaIndex+i]; ok {
+					break
+				}
+			}
+			for i := -1; p.AlphaIndex+i >= 'a'-'a'; i-- {
+				checkStepAndSet(c, &steps, &p, p.NumberIndex, p.AlphaIndex+i)
+				if _, ok := c.Board[p.NumberIndex*9+p.AlphaIndex+i]; ok {
+					break
+				}
+			}
+		case 'c': // 炮
+			var i int
+			for i = 1; p.NumberIndex+i <= 9; i++ {
+				if _, ok := c.Board[(p.NumberIndex+i)*9+p.AlphaIndex]; !ok {
+					steps = append(steps, [4]int{p.NumberIndex, p.AlphaIndex, p.NumberIndex + i, p.AlphaIndex})
+				} else {
+					break
+				}
+			}
+			for i++; p.NumberIndex+i <= 9; i++ {
+				if _, ok := c.Board[(p.NumberIndex+i)*9+p.AlphaIndex]; ok {
+					checkStepAndSet(c, &steps, &p, p.NumberIndex+i, p.AlphaIndex)
+					break
+				}
+			}
+			for i = -1; p.NumberIndex+i >= 0; i-- {
+				if _, ok := c.Board[(p.NumberIndex+i)*9+p.AlphaIndex]; !ok {
+					steps = append(steps, [4]int{p.NumberIndex, p.AlphaIndex, p.NumberIndex + i, p.AlphaIndex})
+				} else {
+					break
+				}
+			}
+			for i--; p.NumberIndex+i >= 0; i-- {
+				if _, ok := c.Board[(p.NumberIndex+i)*9+p.AlphaIndex]; ok {
+					checkStepAndSet(c, &steps, &p, p.NumberIndex+i, p.AlphaIndex)
+					break
+				}
+			}
+			for i = 1; p.AlphaIndex+i <= 'i'-'a'; i++ {
+				if _, ok := c.Board[p.NumberIndex*9+p.AlphaIndex+i]; !ok {
+					steps = append(steps, [4]int{p.NumberIndex, p.AlphaIndex, p.NumberIndex, p.AlphaIndex + i})
+				} else {
+					break
+				}
+			}
+			for i++; p.AlphaIndex+i <= 'i'-'a'; i++ {
+				if _, ok := c.Board[p.NumberIndex*9+p.AlphaIndex+i]; ok {
+					checkStepAndSet(c, &steps, &p, p.NumberIndex, p.AlphaIndex+i)
+					break
+				}
+			}
+			for i = -1; p.AlphaIndex+i >= 'a'-'a'; i-- {
+				if _, ok := c.Board[p.NumberIndex*9+p.AlphaIndex+i]; !ok {
+					steps = append(steps, [4]int{p.NumberIndex, p.AlphaIndex, p.NumberIndex, p.AlphaIndex + i})
+				} else {
+					break
+				}
+			}
+			for i--; p.AlphaIndex+i >= 'a'-'a'; i++ {
+				if _, ok := c.Board[p.NumberIndex*9+p.AlphaIndex+i]; ok {
+					checkStepAndSet(c, &steps, &p, p.NumberIndex, p.AlphaIndex+i)
+					break
+				}
+			}
+		case 'p': // 卒、兵
+			if c.Next == 'b' {
+				if p.NumberIndex == 5 || p.NumberIndex == 6 {
+					checkStepAndSet(c, &steps, &p, p.NumberIndex-1, p.AlphaIndex)
+				} else {
+					for _, r := range PMoveRules {
+						if p.NumberIndex-r[0] >= 0 && p.NumberIndex-r[0] <= 4 && p.AlphaIndex+r[1] >= 'a'-'a' && p.AlphaIndex+r[1] <= 'i'-'a' {
+							checkStepAndSet(c, &steps, &p, p.NumberIndex-r[0], p.AlphaIndex+r[1])
+						}
+					}
+				}
+			} else {
+				if p.NumberIndex == 3 || p.NumberIndex == 4 {
+					checkStepAndSet(c, &steps, &p, p.NumberIndex+1, p.AlphaIndex)
+				} else {
+					for _, r := range PMoveRules {
+						if p.NumberIndex+r[0] >= 5 && p.NumberIndex+r[0] <= 9 && p.AlphaIndex+r[1] >= 'a'-'a' && p.AlphaIndex+r[1] <= 'i'-'a' {
+							checkStepAndSet(c, &steps, &p, p.NumberIndex+r[0], p.AlphaIndex+r[1])
+						}
+					}
+				}
+			}
+		}
+	}
+	return
+}
