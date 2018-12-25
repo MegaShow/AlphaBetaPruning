77711fdfb83b38463557bd3cea8c6417cc295bab
MegaShow <megaxiu@outlook.com>
2018-12-26 00:13:30 +0800

    Fix red win bug

--- chess/search.go
+++ chess/search.go
@@ -3,31 +3,56 @@ package chess
 import (
 	"fmt"
 	"math"
+	"sort"
 	"time"
 )
 
 func Search(chess *Chess) string {
 	start := time.Now()
-	depth := 2
-	if chess.GetPiecesSize() < 6 {
+	depth := 3
+	if chess.GetPiecesSize() < 4 {
+		depth = 6
+	} else if chess.GetPiecesSize() < 8 {
 		depth = 5
-	} else if chess.GetPiecesSize() < 12 {
+	} else if chess.GetPiecesSize() < 16 {
 		depth = 4
-	} else if chess.GetPiecesSize() < 28 {
-		depth = 3
 	}
 	steps := chess.GetNextSteps()
 	alpha, _ := math.MinInt32, math.MaxInt32
 	var s [4]int
+	var m [][5]int
 	for _, step := range steps {
 		move := chess.Move(step)
 		v := AlphaBetaPruning(chess, depth, alpha, math.MaxInt32)
 		// v := Minimax(chess, depth)
+		if v <= -90000 {
+			ksteps := chess.GetNextSteps()
+			var fail bool
+			for _, ks := range ksteps {
+				move := chess.Move(ks)
+				if chess.IsWin('w') {
+					chess.UnMove(move)
+					fail = true
+					break
+				}
+				chess.UnMove(move)
+			}
+			if !fail {
+				m = append(m, [5]int{v, step[0], step[1], step[2], step[3]})
+			}
+		}
 		chess.UnMove(move)
 		if alpha < v {
 			alpha = v
 			s = step
 		}
+	}
+	if len(m) != 0 {
+		sort.Slice(m, func(i, j int) bool {
+			return m[i][0] < m[j][0]
+		})
+		alpha = m[0][0]
+		s = [4]int{m[0][1], m[0][2], m[0][3], m[0][4]}
 	}
 	step := string([]rune{rune(s[1] + 'a'), rune(s[0] + '0'), rune(s[3] + 'a'), rune(s[2] + '0')})
 	fmt.Printf("Value: %d, Step: %s, Pieces: %d, Time: %v\n", alpha, step, chess.GetPiecesSize(), time.Since(start))
